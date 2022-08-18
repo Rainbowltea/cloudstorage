a3a92084a486a9927e29c20d165085e53d144a8d
XiangXuhuai <1506347180@qq.com>
2022-08-18 18:25:51 +0800

    保存文件元信息

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"cloud/meta"
+	"cloud/util"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,19 +27,29 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
 		//创建本地文件来接受文件流
-		newFile, err := os.Create("tmp/" + head.Filename)
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Print("Failed to create file,err:%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
+
 		//将内存中的文件拷贝到新文件的buffer区中
-		_, err = io.Copy(newFile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file,err:%s\n", err.Error())
 			return
 		}
+		//将当前已打开的文件句柄的游标移到文件内容的顶部
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 }
