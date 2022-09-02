ad8d20f3eca0961977b5f6f20da4ba49127eaca8
XiangXuhuai <1506347180@qq.com>
2022-09-02 19:40:50 +0800

    ceph使用测试

---
+++ config/ceph.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	// CephAccessKey : 访问Key
+	CephAccessKey = "43Y5NZSTY51CUOAAPUIY"
+	// CephSecretKey : 访问密钥
+	CephSecretKey = "eVJgPy0PWk9vIb7uWLBWylySDuIw8U4MP4xGFh9s"
+	// CephGWEndpoint : gateway地址
+	CephGWEndpoint = "http://127.0.0.1:7480"
+)
--- test/test_mpupload.go
+++ test/test_mpupload.go
@@ -1,142 +1,142 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
+// import (
+// 	"bufio"
+// 	"bytes"
+// 	"fmt"
+// 	"io"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/url"
+// 	"os"
+// 	"strconv"
 
-	jsonit "github.com/json-iterator/go"
-)
+// 	jsonit "github.com/json-iterator/go"
+// )
 
-func multipartUpload(filename string, targetURL string, chunkSize int) error {
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer f.Close()
+// func multipartUpload(filename string, targetURL string, chunkSize int) error {
+// 	f, err := os.Open(filename)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return err
+// 	}
+// 	defer f.Close()
 
-	bfRd := bufio.NewReader(f)
-	index := 0
+// 	bfRd := bufio.NewReader(f)
+// 	index := 0
 
-	ch := make(chan int)
-	buf := make([]byte, chunkSize) //每次读取chunkSize大小的内容
-	for {
-		n, err := bfRd.Read(buf)
-		if n <= 0 {
-			break
-		}
-		index++
+// 	ch := make(chan int)
+// 	buf := make([]byte, chunkSize) //每次读取chunkSize大小的内容
+// 	for {
+// 		n, err := bfRd.Read(buf)
+// 		if n <= 0 {
+// 			break
+// 		}
+// 		index++
 
-		bufCopied := make([]byte, 5*1048576)
-		copy(bufCopied, buf)
+// 		bufCopied := make([]byte, 5*1048576)
+// 		copy(bufCopied, buf)
 
-		go func(b []byte, curIdx int) {
-			fmt.Printf("upload_size: %d\n", len(b))
+// 		go func(b []byte, curIdx int) {
+// 			fmt.Printf("upload_size: %d\n", len(b))
 
-			resp, err := http.Post(
-				targetURL+"&index="+strconv.Itoa(curIdx),
-				"multipart/form-data",
-				bytes.NewReader(b))
-			if err != nil {
-				fmt.Println(err)
-			}
+// 			resp, err := http.Post(
+// 				targetURL+"&index="+strconv.Itoa(curIdx),
+// 				"multipart/form-data",
+// 				bytes.NewReader(b))
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
 
-			body, er := ioutil.ReadAll(resp.Body)
-			fmt.Printf("%+v %+v\n", string(body), er)
-			resp.Body.Close()
+// 			body, er := ioutil.ReadAll(resp.Body)
+// 			fmt.Printf("%+v %+v\n", string(body), er)
+// 			resp.Body.Close()
 
-			ch <- curIdx
-		}(bufCopied[:n], index)
+// 			ch <- curIdx
+// 		}(bufCopied[:n], index)
 
-		//遇到任何错误立即返回，并忽略 EOF 错误信息
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println(err.Error())
-			}
-		}
-	}
+// 		//遇到任何错误立即返回，并忽略 EOF 错误信息
+// 		if err != nil {
+// 			if err == io.EOF {
+// 				break
+// 			} else {
+// 				fmt.Println(err.Error())
+// 			}
+// 		}
+// 	}
 
-	for idx := 0; idx < index; idx++ {
-		select {
-		case res := <-ch:
-			fmt.Println(res)
-		}
-	}
+// 	for idx := 0; idx < index; idx++ {
+// 		select {
+// 		case res := <-ch:
+// 			fmt.Println(res)
+// 		}
+// 	}
 
-	return nil
+// 	return nil
 
-}
+// }
 
-func main() {
-	username := "admin"
-	token := "54eefa7dbd5bcf852c52fecd816f2a315c61832c"
-	filehash := "dfa39cac093a7a9c94d25130671ec474d51a2995"
+// func main() {
+// 	username := "admin"
+// 	token := "54eefa7dbd5bcf852c52fecd816f2a315c61832c"
+// 	filehash := "dfa39cac093a7a9c94d25130671ec474d51a2995"
 
-	// 1. 请求初始化分块上传接口
-	resp, err := http.PostForm(
-		"http://localhost:8080/file/mpupload/init",
-		url.Values{
-			"username": {username},
-			"token":    {token},
-			"filehash": {filehash},
-			"filesize": {"148796421"},
-		})
+// 	// 1. 请求初始化分块上传接口
+// 	resp, err := http.PostForm(
+// 		"http://localhost:8080/file/mpupload/init",
+// 		url.Values{
+// 			"username": {username},
+// 			"token":    {token},
+// 			"filehash": {filehash},
+// 			"filesize": {"148796421"},
+// 		})
 
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		os.Exit(-1)
+// 	}
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
+// 	defer resp.Body.Close()
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		os.Exit(-1)
+// 	}
 
-	// 2. 得到uploadID以及服务端指定的分块大小chunkSize
-	uploadID := jsonit.Get(body, "data").Get("UploadID").ToString()
-	chunkSize := jsonit.Get(body, "data").Get("ChunkSize").ToInt()
-	fmt.Printf("uploadid: %s  chunksize: %d\n", uploadID, chunkSize)
+// 	// 2. 得到uploadID以及服务端指定的分块大小chunkSize
+// 	uploadID := jsonit.Get(body, "data").Get("UploadID").ToString()
+// 	chunkSize := jsonit.Get(body, "data").Get("ChunkSize").ToInt()
+// 	fmt.Printf("uploadid: %s  chunksize: %d\n", uploadID, chunkSize)
 
-	// 3. 请求分块上传接口
-	filename := "/usr/download/go1.19.linux-amd64.tar.gz"
-	tURL := "http://localhost:8080/file/mpupload/uppart?" +
-		"username=admin&token=" + token + "&uploadid=" + uploadID
-	multipartUpload(filename, tURL, chunkSize)
+// 	// 3. 请求分块上传接口
+// 	filename := "/usr/download/go1.19.linux-amd64.tar.gz"
+// 	tURL := "http://localhost:8080/file/mpupload/uppart?" +
+// 		"username=admin&token=" + token + "&uploadid=" + uploadID
+// 	multipartUpload(filename, tURL, chunkSize)
 
-	// 4. 请求分块完成接口
-	resp, err = http.PostForm(
-		"http://localhost:8080/file/mpupload/complete",
-		url.Values{
-			"username": {username},
-			"token":    {token},
-			"filehash": {filehash},
-			"filesize": {"148796421"},
-			"filename": {""},
-			"uploadid": {uploadID},
-		})
+// 	// 4. 请求分块完成接口
+// 	resp, err = http.PostForm(
+// 		"http://localhost:8080/file/mpupload/complete",
+// 		url.Values{
+// 			"username": {username},
+// 			"token":    {token},
+// 			"filehash": {filehash},
+// 			"filesize": {"148796421"},
+// 			"filename": {""},
+// 			"uploadid": {uploadID},
+// 		})
 
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		os.Exit(-1)
+// 	}
 
-	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
-	fmt.Printf("complete result: %s\n", string(body))
+// 	defer resp.Body.Close()
+// 	body, err = ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		os.Exit(-1)
+// 	}
+// 	fmt.Printf("complete result: %s\n", string(body))
 
-}
+// }
